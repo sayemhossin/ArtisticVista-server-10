158a98d999c7512ccbd5472c5a98fa937c35bc26
sayemhossin <sayemhossin22@gmail.com>
2024-04-27 01:55:45 +0600

    added craft item on database

--- index.js
+++ index.js
@@ -32,12 +32,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+  const database = client.db("ArtisticVista")
+  const craftCollection = database.collection("craft")
+
+
+
+app.post('/crafts', async(req,res)=>{
+    const craft = req.body
+    const result = await craftCollection.insertOne(craft)
+    res.send(result)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
